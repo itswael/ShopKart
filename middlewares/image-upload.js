93affe62563d864bb1f6ed76b4cfbef43b860ede
Mohd Wael <Mohd.wael.cs.2016@miet.ac.in>
2022-10-29 21:50:44 +0530

    added error handling middlewares

--- middlewares/image-upload.js
+++ middlewares/image-upload.js
@@ -1,18 +1,16 @@
 const multer = require('multer');
-const uuid = require('uuid').v4;
-
+//const uuid = require('uuid').v4;
+const { v4: uuid } = require('uuid');
 
 const upload = multer({
-    storage: multer.diskStorage({
-        destination: 'product-data/images',
-        filename: function(req, file, cb){
-            cb(null, uuid() + '-' +file.originalname );
-        }
-    })
+  storage: multer.diskStorage({
+    destination: 'product-data/images',
+    filename: function(req, file, cb) {
+      cb(null, uuid() + '-' + file.originalname);
+    }
+  })
 });
 
 const configuredMulterMiddleware = upload.single('image');
 
-module.exports = {
-    configuredMulterMiddleware: configuredMulterMiddleware
-}
+module.exports = configuredMulterMiddleware;
