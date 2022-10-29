5b461574321d5d022f68dc450dfe9efcb6a32995
Mohd Wael <Mohd.wael.cs.2016@miet.ac.in>
2022-10-30 00:03:54 +0530

    finished code

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,11 +4,11 @@ const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.get('/signup',authController.getSignUp);
+router.get('/signup', authController.getSignup);
 
 router.post('/signup', authController.signup);
 
-router.get('/login',authController.getLogin);
+router.get('/login', authController.getLogin);
 
 router.post('/login', authController.login);
 
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
-const baseController = require('../controllers/base.controller');
-
 const router = express.Router();
 
-router.get('/', baseController.getProducts);
+router.get('/', function(req, res) {
+  res.redirect('/products');
+});
+
+router.get('/401', function(req, res) {
+  res.status(401).render('shared/401');
+});
+
+router.get('/403', function(req, res) {
+  res.status(403).render('shared/403');
+});
 
 module.exports = router;
