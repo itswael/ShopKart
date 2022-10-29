08df0e9d49e3ec8efe017578bceb00cc97127aa3
Mohd Wael <Mohd.wael.cs.2016@miet.ac.in>
2022-10-29 21:46:32 +0530

    updated and added middleware for session and authentication

--- app.js
+++ app.js
@@ -1,50 +1,62 @@
 const path = require('path');
 
 const express = require('express');
-
 const csrf = require('csurf');
 const expressSession = require('express-session');
 
 const createSessionConfig = require('./config/session');
-
 const db = require('./data/database');
-
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
-const checkAuthStatusmiddleware = require('./middlewares/check-auth');
-const addCsrfTokenMiddleware = require('./middlewares/csrf_token');
-
+const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
+const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
+const notFoundMiddleware = require('./middlewares/not-found');
 const authRoutes = require('./routes/auth.routes');
-const baseRoutes = require('./routes/base.routes');
 const productsRoutes = require('./routes/products.routes');
-const adminRoutes = require('./routes/admin.routes')
+const baseRoutes = require('./routes/base.routes');
+const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
+const ordersRoutes = require('./routes/orders.routes');
+
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+app.use('/products/assets', express.static('product-data'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const sessionConfig = createSessionConfig();
-app.use(expressSession(sessionConfig));
 
+app.use(expressSession(sessionConfig));
 app.use(csrf());
 
-app.use(addCsrfTokenMiddleware);
-app.use(checkAuthStatusmiddleware);
+app.use(cartMiddleware);
+app.use(updateCartPricesMiddleware);
 
-app.use(authRoutes);
+app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
+
 app.use(baseRoutes);
+app.use(authRoutes);
 app.use(productsRoutes);
-app.use('/admin',adminRoutes)
+app.use('/cart', cartRoutes);
+app.use('/orders', protectRoutesMiddleware, ordersRoutes);
+app.use('/admin', protectRoutesMiddleware, adminRoutes);
+
+app.use(notFoundMiddleware);
 
 app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
-.then(function(){
+  .then(function () {
     app.listen(3000);
-})
-.catch(function(error){
-    console.log("failed to connect to database!");
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database!');
     console.log(error);
-});
+  });
