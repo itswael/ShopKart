552d1b2bb06e414b09dd70debc6a450a577ffd0d
Mohd Wael <Mohd.wael.cs.2016@miet.ac.in>
2022-10-29 21:43:40 +0530

    added order and product management for admin and customers

--- models/user.model.js
+++ models/user.model.js
@@ -1,43 +1,55 @@
 const bcrypt = require('bcryptjs');
+const mongodb = require('mongodb');
+
 const db = require('../data/database');
 
-class User{
-    constructor(email, password, fullname, street, postal, city){
-        this.email = email;
-        this.password = password;
-        this.fullname = fullname;
-        this.address = {
-            street: street,
-            postal: postal,
-            city: city
-        }
-    }
+class User {
+  constructor(email, password, fullname, street, postal, city) {
+    this.email = email;
+    this.password = password;
+    this.name = fullname;
+    this.address = {
+      street: street,
+      postalCode: postal,
+      city: city,
+    };
+  }
 
-    getUserWithSameEmail(){
-        return db.getDb().collection('users').findOne({email: this.email});
-    }
+  static findById(userId) {
+    const uid = new mongodb.ObjectId(userId);
 
-    async existsAlready(){
-        const exsistingUser = await this.getUserWithSameEmail();
-        if(exsistingUser){
-            return true;
-        }
-        return false;
-    }
+    return db
+      .getDb()
+      .collection('users')
+      .findOne({ _id: uid }, { projection: { password: 0 } });
+  }
 
-   async signup(){
-        const hashedPassword = await bcrypt.hash(this.password, 12);
-        await db.getDb().collection('users').insertOne({
-            email: this.email,
-            password: hashedPassword,
-            fullname: this.fullname,
-            address: this.address
-        });
-    }
+  getUserWithSameEmail() {
+    return db.getDb().collection('users').findOne({ email: this.email });
+  }
 
-    hasMatchingPassword(hashedPassword){
-        return bcrypt.compare(this.password, hashedPassword);
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
     }
+    return false;
+  }
+
+  async signup() {
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+
+    await db.getDb().collection('users').insertOne({
+      email: this.email,
+      password: hashedPassword,
+      name: this.name,
+      address: this.address,
+    });
+  }
+
+  hasMatchingPassword(hashedPassword) {
+    return bcrypt.compare(this.password, hashedPassword);
+  }
 }
 
 module.exports = User;
