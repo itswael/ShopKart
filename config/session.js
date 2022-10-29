493cc7a7bf575a98f8bfffcde9a34e65049b4009
Mohd Wael <Mohd.wael.cs.2016@miet.ac.in>
2022-10-29 19:22:51 +0530

    updated session and db details

--- config/session.js
+++ config/session.js
@@ -1,27 +1,28 @@
 const expressSession = require('express-session');
 const mongoDbStore = require('connect-mongodb-session');
 
-function createSessionStore(){
-    const mongoDBStore = mongoDbStore(expressSession);
-    const store = new mongoDBStore({
-        uri: 'mongodb://127.0.0.1:27017',
-        databaseName: 'shopkart',
-        collection: 'sessions'
-    });
+function createSessionStore() {
+  const MongoDBStore = mongoDbStore(expressSession);
 
-    return store;
+  const store = new MongoDBStore({
+    uri: 'mongodb://127.0.0.1:27017',
+    databaseName: 'shopkart',
+    collection: 'sessions'
+  });
+
+  return store;
 }
 
-function createSessionConfig(){
-    return {
-        secret: 'super-secret',
-        resave: false,
-        saveUninitialized: false,
-        store: createSessionStore(),
-        cookie: {
-            maxAge: 2*24*60*60*1000
-        }
-    };
+function createSessionConfig() {
+  return {
+    secret: 'super-secret',
+    resave: false,
+    saveUninitialized: false,
+    store: createSessionStore(),
+    cookie: {
+      maxAge: 2 * 24 * 60 * 60 * 1000
+    }
+  };
 }
 
 module.exports = createSessionConfig;
